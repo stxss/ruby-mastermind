061d42ebdbcbd3d8d4b57790056dc8cf9bafd076
stxss <93729036+stxss@users.noreply.github.com>
2023-02-04 17:47:07 +0000

    Delete todo

--- main.rb
+++ main.rb
@@ -8,12 +8,3 @@ require './board'
 
 Game.new
 
-# TODO- CL game ; 12 turns to guess secret code; starts with guessing computer's random code
-# * Game randomly selects a code (colors) and human must guess
-
-# TODO- Have to give proper feedback on the guesses
-# TODO - Refactor the code to allow the human player to choose whether they want to create or guess the code
-# TODO - Build it out so the computer has to guess, in the case of the human player being the code creator
-
-# TODO- Prompt for allowing duplicates/blanks
-# Todo- this means 4 different sets of numbers
